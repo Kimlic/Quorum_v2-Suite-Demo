1a4442fe86252fecc731202ade737df793b3e2f0
Dmytro Nasyrov <dmytro@pharosproduction.com>
2018-06-07 13:23:06 +0300

    replace dockerimage with docker hub image

--- Quorum/lib/run_docker_container.sh
+++ Quorum/lib/run_docker_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -v $(pwd)/$line:/${PWD##*/}  -w /${PWD##*/} quorum-2.0.2:latest bash
+docker run -it -v $(pwd)/$line:/${PWD##*/}  -w /${PWD##*/} kimlictr/quorum:latest bash
